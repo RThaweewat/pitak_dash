bad8be60429011402a3b82939e2422755aa70d12
Thaweewat <thaweewat.data@gmail.com>
2023-08-09 05:10:39 +0700

    test

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import folium
 from folium.plugins import HeatMap
 
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data()
 def load_data():
     return pd.read_csv("result.csv")
+
 
 data = load_data()
 
@@ -21,8 +22,8 @@ feature = st.selectbox(
 
 # Get unique times and create a time range dropdown
 times = sorted(data["time"].unique())
-start_time = st.selectbox('Select start time:', times, index=0, key="start_time")
-end_time = st.selectbox('Select end time:', times, index=len(times)-1, key="end_time")
+start_time = st.selectbox("Select start time:", times, index=0, key="start_time")
+end_time = st.selectbox("Select end time:", times, index=len(times) - 1, key="end_time")
 
 # Ensure that the end time is always after the start time
 if start_time >= end_time:
@@ -32,20 +33,22 @@ if start_time >= end_time:
 filtered_data = data[(data["time"] >= start_time) & (data["time"] <= end_time)]
 
 # Average risk (assuming 'proba' column represents risk)
-average_risk = filtered_data['proba'].mean()
+average_risk = filtered_data["proba"].mean()
 st.write(f"Average risk for selected time period: {average_risk}")
 
-m = folium.Map(location=[13.6773, 100.4555], zoom_start=19, tiles="OpenStreetMap", max_zoom=30)
+m = folium.Map(
+    location=[13.6773, 100.4555], zoom_start=19, tiles="OpenStreetMap", max_zoom=30
+)
 
 heatmap_data = []
 
 # Instead of directly computing mean after grouping, select necessary columns first
-numeric_cols = ['lat', 'long', 'temperature', 'humidity', 'gas_smoke', 'proba']
-grouped_data = filtered_data.groupby('no_board')[numeric_cols].mean()
+numeric_cols = ["lat", "long", "temperature", "humidity", "gas_smoke", "proba"]
+grouped_data = filtered_data.groupby("no_board")[numeric_cols].mean()
 
 # Loop to add board data to heatmap_data list for the chosen time period and add clickable markers
 for index, row in grouped_data.iterrows():
-    lat, long = row['lat'], row['long']
+    lat, long = row["lat"], row["long"]
     value = row[feature]
     heatmap_data.append([lat, long, value])
 
@@ -67,5 +70,4 @@ folium_static(m)
 
 # title for streamlit line chart
 st.title("Risk Probability")
-st.line_chart(filtered_data.set_index("time")['proba', 'no_board'])
-
+st.line_chart(filtered_data.set_index("time")["proba"])
