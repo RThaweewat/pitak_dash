4e923a32dd8e34ede1b89f553b1333da9b714a1b
Thaweewat <thaweewat.data@gmail.com>
2023-08-09 04:47:07 +0700

    test

--- main.py
+++ main.py
@@ -7,41 +7,13 @@ from folium.plugins import HeatMap
 import re
 
 
-def dms_to_decimal(dms_lat, dms_long):
-    """Convert DMS (Degrees, Minutes, Seconds) to decimal format for latitude and longitude."""
-    def dms2dd(dms):
-        degrees, minutes, direction = 0, 0, 1
-        dms_match = re.match(r'(\d+)°(\d+)\'([\d.]+)"([NSEW])', dms)
-        if dms_match:
-            degrees = float(dms_match.group(1))
-            minutes = float(dms_match.group(2))
-            seconds = float(dms_match.group(3))
-            if dms_match.group(4) in ['S', 'W']:
-                direction = -1
-        return direction * (degrees + minutes/60 + seconds/3600)
-
-    lat = dms2dd(dms_lat)
-    long = dms2dd(dms_long)
-    return lat, long
-
-
-@st.cache_data
+# Load data
+@st.cache
 def load_data():
     return pd.read_csv("result.csv")
 
 
 data = load_data()
-
-location_dict = {
-    1: ["13°40'38.3\"N", "100°27'19.6\"E"],
-    2: ["3°40'38.5\"N", "100°27'19.6\"E"],
-    3: ["13°40'38.5\"N", "100°27'19.8\"E"],
-    4: ["13°40'38.3\"N", "100°27'19.8\"E"]
-}
-
-# Convert the location_dict to decimal
-for key, coords in location_dict.items():
-    location_dict[key] = dms_to_decimal(coords[0], coords[1])
 
 # Streamlit UI
 st.title("Interactive Heat Map on Folium")
@@ -54,14 +26,12 @@ feature = st.selectbox(
 times = sorted(data["time"].unique())
 start_time = st.selectbox('Select start time:', times, index=0, key="start_time")
 end_time = st.selectbox('Select end time:', times, index=len(times)-1, key="end_time")
-
+filtered_data = data[(data["time"] >= start_time) & (data["time"] <= end_time)]
 
 # Ensure that the end time is always after the start time
 if start_time >= end_time:
     st.warning("End time must be after start time!")
     st.stop()
-
-filtered_data = data[(data["time"] >= start_time) & (data["time"] <= end_time)]
 
 # Average risk (assuming 'proba' column represents risk)
 average_risk = filtered_data['proba'].mean()
@@ -84,10 +54,10 @@ for index, row in filtered_data.groupby('no_board').mean().iterrows():  # Groupi
     heatmap_data.append([lat, long, value])
 
     popup_content = f"""
-    Board No: {index}<br>
-    Temperature: {row['temperature']}°C<br>
-    Humidity: {row['humidity']}%<br>
-    Risk Probability: {row['proba']}
+    Board No: {round(index, 2)}<br>
+    Temperature: {round(row['temperature'], 2)}°C<br>
+    Humidity: {round(row['humidity'], 2)}%<br>
+    Risk Probability: {round(row['proba'], 2)}
     """
 
     folium.Marker(
