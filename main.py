9f9c0dd532f4725b3fdfdc8f9634668695c0ed77
Thaweewat <thaweewat.data@gmail.com>
2023-08-09 04:42:20 +0700

    Refactored data filter and heatmap generation
    This rework updates the data filtering and heatmap generation process in the main.py of pitak_dash project. Previously, we were manually converting coordinate formats and selecting time ranges. However, this commit introduces a time input widget for easier and more precise time selection. Additionally, it simplifies the heatmap data generation by directly using grouped means from the filtered data set instead of looping through every record. These changes lead to a more user-friendly interface and streamlined, efficient code execution.

--- main.py
+++ main.py
@@ -4,97 +4,50 @@ import datetime
 from streamlit_folium import folium_static
 import folium
 from folium.plugins import HeatMap
-import re
 
-
-def dms_to_decimal(dms_lat, dms_long):
-    """Convert DMS (Degrees, Minutes, Seconds) to decimal format for latitude and longitude."""
-    def dms2dd(dms):
-        degrees, minutes, direction = 0, 0, 1
-        dms_match = re.match(r'(\d+)°(\d+)\'([\d.]+)"([NSEW])', dms)
-        if dms_match:
-            degrees = float(dms_match.group(1))
-            minutes = float(dms_match.group(2))
-            seconds = float(dms_match.group(3))
-            if dms_match.group(4) in ['S', 'W']:
-                direction = -1
-        return direction * (degrees + minutes/60 + seconds/3600)
-
-    lat = dms2dd(dms_lat)
-    long = dms2dd(dms_long)
-    return lat, long
-
-
-@st.cache_data
+# Load data
+@st.cache
 def load_data():
     return pd.read_csv("result.csv")
 
-
 data = load_data()
 
-location_dict = {
-    1: ["13°40'38.3\"N", "100°27'19.6\"E"],
-    2: ["3°40'38.5\"N", "100°27'19.6\"E"],
-    3: ["13°40'38.5\"N", "100°27'19.8\"E"],
-    4: ["13°40'38.3\"N", "100°27'19.8\"E"],
-}
+# Create a time input widget for start and end times with minute precision
+start_time = st.time_input('Select start time:', datetime.time(0, 0)).strftime('%H:%M')
+end_time = st.time_input('Select end time:', datetime.time(23, 59)).strftime('%H:%M')
 
-# Convert the location_dict to decimal
-for key, coords in location_dict.items():
-    location_dict[key] = dms_to_decimal(coords[0], coords[1])
+# Filter data based on the chosen times
+filtered_data = data[(data["time"].str.slice(0, 5) >= start_time) & (data["time"].str.slice(0, 5) <= end_time)]
 
 # Streamlit UI
 st.title("Interactive Heat Map on Folium")
 
+# Dropdown menu
 feature = st.selectbox(
     "Choose a feature to plot:", ["temperature", "humidity", "gas_smoke", "proba"]
 )
 
-# Get unique times and create a time range dropdown
-times = sorted(data["time"].unique())
-start_time = st.selectbox('Select start time:', times, index=0, key="start_time")
-end_time = st.selectbox('Select end time:', times, index=len(times)-1, key="end_time")
-
-
-# Ensure that the end time is always after the start time
-if start_time >= end_time:
-    st.warning("End time must be after start time!")
-    st.stop()
-
-filtered_data = data[(data["time"] >= start_time) & (data["time"] <= end_time)]
-
-# Average risk (assuming 'proba' column represents risk)
-average_risk = filtered_data['proba'].mean()
-st.write(f"Average risk for selected time period: {average_risk}")
-
-# sat map instead
-m = folium.Map(location=[13.6773, 100.4554], zoom_start=22, tiles="OpenStreetMap", max_zoom=30)
+m = folium.Map(location=[13.6773, 100.456], zoom_start=20, tiles="OpenStreetMap", max_zoom=30)
 
 heatmap_data = []
 
-# Loop to add all the board data to heatmap_data list for the chosen time period and add clickable markers
-for no_board, coord in location_dict.items():
-    lat, long = coord[0], coord[1]
-    board_data = filtered_data[filtered_data['no_board'] == no_board]
-    if not board_data.empty:
-        value = board_data[feature].mean()  # Average value for the board across time
-        heatmap_data.append([lat, long, value])
+# Loop to add board data to heatmap_data list for the chosen time period and add clickable markers
+for index, row in filtered_data.groupby('no_board').mean().iterrows():  # Grouping by no_board and taking mean values
+    lat, long = row['lat'], row['long']
+    value = row[feature]
+    heatmap_data.append([lat, long, value])
 
-        # Extract the required info for the popup
-        temperature = board_data["temperature"].mean()
-        humidity = board_data["humidity"].mean()
-        proba = board_data["proba"].mean()
+    popup_content = f"""
+    Board No: {index}<br>
+    Temperature: {row['temperature']}°C<br>
+    Humidity: {row['humidity']}%<br>
+    Risk Probability: {row['proba']}
+    """
 
-        popup_content = f"""
-        Temperature: {temperature}°C<br>
-        Humidity: {humidity}%<br>
-        Risk Probability: {proba}
-        """
-
-        folium.Marker(
-            [lat, long],
-            popup=popup_content,
-        ).add_to(m)
+    folium.Marker(
+        [lat, long],
+        popup=popup_content,
+    ).add_to(m)
 
 # Add heatmap
 HeatMap(heatmap_data, radius=20).add_to(m)
